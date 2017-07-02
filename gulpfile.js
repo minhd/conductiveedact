6fa286e3037e4b88918500580e8bb6ba366282f9
Minh Duc Nguyen <dekarvn@gmail.com>
2017-07-02 17:59:07 +1000

    dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,46 +4,96 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let server = require('gulp-server-livereload');
 let cleanCSS = require('gulp-clean-css');
+let concat = require('gulp-concat');
+let concatCss = require('gulp-concat-css');
+let minify = require('gulp-minify');
 
 let config = {
-  libdir: './node_modules'
+  dir : {
+    lib : './node_modules',
+    src: './src',
+    dist: './dist'
+  }
 }
 
+gulp.task('static-files', () => {
+  gulp.src(config.dir.src + '/images/*.*')
+    .pipe(gulp.dest(config.dir.dist + '/images'));
+  return gulp.src(config.dir.src + '/*.html')
+    .pipe(gulp.dest(config.dir.dist));
+});
+
 gulp.task('sass', function () {
-  return gulp.src('./assets/sass/**/*.scss')
+  return gulp.src(config.dir.src + '/assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest(config.dir.dist + '/assets/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+  gulp.watch(config.dir.src + '/assets/sass/**/*.scss', ['sass', 'concat-styles']);
+});
+
+gulp.task('concat-styles', ['sass', 'static-files'], () => {
+  return gulp.src([
+    config.dir.src + '/assets/css/font-awesome.min.css',
+    config.dir.dist + '/assets/css/main.css'
+  ]).pipe(concatCss('bundle.css'))
+    .pipe(gulp.dest(config.dir.dist + '/assets/css'));
 });
 
 gulp.task('fonts', function() {
-  return gulp.src(config.libdir + '/font-awesome/fonts/**.*')
-    .pipe(gulp.dest('./assets/fonts'));
+  return gulp.src(config.dir.lib + '/font-awesome/fonts/**.*')
+    .pipe(gulp.dest(config.dir.dist +'/assets/fonts'));
 });
 
-gulp.task('minify-css', () => {
-  return gulp.src('./assets/css/*.css')
+gulp.task('minify-css', ['concat-styles'], () => {
+  return gulp.src(config.dir.dist + '/assets/css/bundle.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest(config.dir.dist + '/assets/css'));
 });
 
-gulp.task('styles', ['fonts', 'sass', 'minify-css']);
+gulp.task('js-lib', () => {
+  return gulp.src([
+    config.dir.lib + '/jquery/dist/jquery.min.js',
+    config.dir.src + '/assets/js/skel.min.js'
+  ]).pipe(concat('lib.js'))
+    .pipe(gulp.dest(config.dir.dist + '/assets/js'));
+});
+
+gulp.task('js-ie', () => {
+  return gulp.src(config.dir.src + '/assets/js/ie/*.js')
+    .pipe(gulp.dest(config.dir.dist + '/assets/js/ie'));
+});
+
+gulp.task('js-main', () => {
+  return gulp.src(config.dir.src + '/assets/js/main.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        }
+    }))
+    .pipe(gulp.dest(config.dir.dist + '/assets/js'));
+});
+
+gulp.task('js:watch', () => {
+  gulp.watch(config.dir.src + '/assets/js/**/*.js', ['js']);
+});
+
+gulp.task('js', ['js-lib', 'js-ie', 'js-main']);
 
 gulp.task('webserver', function() {
-  gulp.src('./')
+  gulp.src('dist')
     .pipe(server({
       host: 'localhost',
       port: 8001,
       open: true,
       livereload: true,
-      directoryListing: false,
+      //directoryListing: false,
     }));
 });
 
-gulp.task('watch', ['sass:watch']);
-gulp.task('build', ['styles']);
+gulp.task('watch', ['sass:watch', 'js:watch']);
+gulp.task('build', ['fonts', 'sass', 'js', 'static-files', 'concat-styles', 'minify-css']);
 gulp.task('default', ['build', 'webserver', 'watch']);
 gulp.task('serve', ['build', 'webserver']);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,8 +19,6 @@
 			}, 100);
 		});
 
-		$('form').placeholder();
-
 		skel.on('+medium -medium', function() {
 			$.prioritize(
 				'.important\\28 medium\\29',
